b1cdb4eea64b4d153f38db23f107334fcdbf624e
Dave <dave.imbriaco@gmail.com>
2014-04-30 10:52:50 -0400

    Pages Push

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+    	$('.ryu-still').hide();
+		$('.ryu-ready').show();
+  })
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+		console.log('line 10');
+	})
+	 .mousedown(function() {
+	 	playHadouken();
+    	$('.ryu-ready').hide();
+    	$('.ryu-throwing').show();
+    	$('.hadouken').finish().show()
+    	.animate({'left': '300px'}, 500, function() {
+    		$(this).hide();
+    		$(this).css('left', '-212px');
+  		})
+  	})
+  	.mouseup(function() {
+    	$('.ryu-throwing').hide();
+    	$('ryu-ready').show();
+    	console.log('line 26');
+  });
+  	$(document).keydown(function() {
+  		var keyCode = (event.keycode ? event.keyCode : event.which);
+  		console.log(keyCode)
+  		if (keyCode == 88) {
+  			$('.ryu-ready').hide();
+  			$('.ryu-cool').show();
+        //.keyup(function) {
+        //  $(.'ryu-cool').hide();
+        //  $('.ryu-ready').show();
+        //}
+  		}
+  	})
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
